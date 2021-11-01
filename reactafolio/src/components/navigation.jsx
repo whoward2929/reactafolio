d2441a0f38667c3ae6796e3348aa7a3564464ffa
whoward2929 <Whoward939@gmail.com>
2021-10-31 20:10:18 -0400

    added navigation.css and jsx

---
+++ reactafolio/src/components/navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <ul>
+            <li>
+                <a href="#about-me">About Me</a>
+            </li>
+            <li>
+                <a href="#work">Projects</a>
+            </li>
+            <li>
+                <a href="#contact">Contact Me</a>
+            </li>
+            <li>
+                <a href="#resume">Resume</a>
+            </li>
+        </ul>
+    )
+}
+
+export default Navigation
