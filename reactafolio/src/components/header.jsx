8e15d33e04cd1e2edf1e3ac32c99e284aac7be60
whoward2929 <Whoward939@gmail.com>
2021-10-31 20:09:57 -0400

    added header.css and .jsx

---
+++ reactafolio/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./navigation";
+
+const Header = () => {
+    return (
+        <div>
+            <p>WKH</p>
+            <Navigation />
+        </div>
+    )
+}
+
+export default Header
