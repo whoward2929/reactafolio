05db10cd0b78801c4c1e7ee5a1b76b8a5f4e9262
whoward2929 <Whoward939@gmail.com>
2021-11-07 20:54:34 -0600

    added body format

--- reactafolio/src/App.js
+++ reactafolio/src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 
 import Navigation from './components/Navigation';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Portfolio/>
     </div>
   );
 }
+
 
 
 
